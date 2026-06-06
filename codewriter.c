776e0c316c00c5284002581044083ed0cebe177f
ThalesAymar <thales_fortes126@hotmail.com>
2026-06-06 14:02:13 -0300

    codewriter.c: Adicionado implementação de writePop()
    writePop escreve o código hack assembly em um arquivo de saida para as linhas de código vm da forma "pop segment value"

--- codewriter.c
+++ codewriter.c
@@ -76,6 +76,41 @@ void writePush(char* segment, int value, CodeWriter* w ){
 
 
 
-void writePop(char* segment, int value, CodeWriter* w ){}
+void writePop(char* segment, int value, CodeWriter* w ){
+	if(strcmp(segment,"temp") == 0 || strcmp(segment,"pointer") == 0){
+        fprintf(w->out,"@SP			// pop %s %d\n", segment, value);
+        fprintf(w->out,"M=M-1\n");
+        fprintf(w->out,"A=Mv");
+        fprintf(w->out,"D=M\n");
+        fprintf(w->out,"@%d\n", getBaseAddress(segment) + value);
+        fprintf(w->out,"M=D\n");
+    }
+    else if(strcmp(segment,"static") == 0){
+    	fprintf(w->out,"@SP			// pop %s %d\n", segment, value);
+        fprintf(w->out,"M=M-1\n");
+        fprintf(w->out,"A=M\n");
+        fprintf(w->out,"D=M\n");
+        fprintf(w->out,"@%d\n", getBaseAddress(segment) + value);
+        fprintf(w->out,"M=D\n");
+	}
+	
+	else{
+		fprintf(w->out,"@%d		// pop %s %d\n", getBaseAddress(segment), segment, value);
+        fprintf(w->out,"D=M\n");
+        fprintf(w->out,"@%d\n", value);
+        fprintf(w->out,"D=D+A\n");
+        fprintf(w->out,"@R13\n");
+        fprintf(w->out,"M=D\n");
+        fprintf(w->out,"@SP\n");
+        fprintf(w->out,"M=M-1\n");
+        fprintf(w->out,"A=M\n");
+        fprintf(w->out,"D=M\n");
+        fprintf(w->out,"@R13\n");
+        fprintf(w->out,"A=M\n");
+        fprintf(w->out,"M=D\n");
+	}
+}
+
+
 
 void writeArithmetic(char* segment, CodeWriter* w){}
