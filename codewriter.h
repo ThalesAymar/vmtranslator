8d61cd0ba15a99c166196850d731139f30133227
ThalesAymar <thales_fortes126@hotmail.com>
2026-06-06 17:39:00 -0300

    Update codewriter.h
    Adicionado declaração das funções para a escrita do código assembler para os comandos vm "add", "sub", "or", e "and".

--- codewriter.h
+++ codewriter.h
@@ -12,7 +12,17 @@ void writePop(char* segment, int value, CodeWriter* w );
 
 void writeArithmetic(char* segment, CodeWriter* w);
 
+
 int getBaseAddress(char* segment);
+
+
+void writeArithmeticAdd(CodeWriter* w);
+
+void writeArithmeticSub(CodeWriter* w);
+
+void writeArithmeticAnd(CodeWriter* w);
+
+void writeArithmeticOr(CodeWriter* w);
 
 
 #endif
